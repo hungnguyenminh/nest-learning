3e28aab1216affb2f355a5a381704ab065976134
nguyenminhhung <hungnm.17k2@gmail.com>
2024-09-24 23:47:15 +0700

    connect db mysql using forRootAsync, init user entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from '@/users/users.module';
 
 @Module({
-  imports: [UsersModule, TasksModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Tạo biến môi trường có thể sử dụng toàn cục
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule], // Đảm bảo ConfigModule đã được import
+      inject: [ConfigService], // Inject ConfigService để lấy các giá trị từ .env
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'], // Đường dẫn đến entity
+        synchronize: true, // Chỉ nên bật ở môi trường phát triển, không dùng trong production
+      }),
+    }),
+    UsersModule,
+  ],
 })
 export class AppModule {}
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,13 @@
-export class User {}
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+}
